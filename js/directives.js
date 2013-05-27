2cdcfa17334d1cfcc3a665b76b3a63eedee0a637
Martin Naumann <martin.naumann@centralway.com>
2013-05-27 11:08:14 +0200

    Directives ftw

--- js/directives.js
+++ js/directives.js
@@ -1,1 +1,18 @@
-angular.module('Direxio.directives').directive();
+window.Direxio.directive('connection', function() {
+    return {
+        restrict: 'E',
+        scope: { connection: '@data'},
+        controller: function($scope, $element, $attrs) {
+/*            $scope.$watch('connection', function(connection) {
+                $scope.connection = JSON.parse(connection).displayText;
+            });*/
+        },
+        link: function(scope, element, attrs) {
+            attrs.$observe('data', function(connection) {
+                scope.connection = JSON.parse(connection);
+            });
+        },
+        template: '<div>{{connection.displayText}}</div>',
+        replace: true
+    };
+});
