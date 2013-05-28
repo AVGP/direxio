1c535f8313702ea53737cdb051511191840c6225
Martin Naumann <martin.naumann@centralway.com>
2013-05-28 15:45:46 +0200

    Adding spinner for loading time

--- js/controller.js
+++ js/controller.js
@@ -7,13 +7,16 @@ function AppCtrl($scope, $location) {
 function DirectionsCtrl($scope, directionsSvc) {
     $scope.destination = "Zurich HB";
     $scope.connections = [];
+    $scope.isLoading = false;
     
     $scope.findConnections = function() {
+        $scope.isLoading = true;
         if(window.webkitNotifications && window.webkitNotifications.checkPermission() !== 0) {
             window.webkitNotifications.requestPermission();
         }
         
         directionsSvc.getConnectionsFromHereTo($scope.destination, function(connections) {
+            $scope.isLoading = false;
             console.log(connections)
             $scope.connections = connections;
         });
