41733ca0d1438e124df264ca427e701e0614c8ba
Martin Naumann <martin.naumann@centralway.com>
2013-06-21 08:43:21 +0200

    Prevent stupid preflights

--- js/app.js
+++ js/app.js
@@ -4,7 +4,10 @@
 // Declare app level module which depends on filters, and services
 window.Direxio = angular.module('DirexioApp', 
 	['Centralway.lungo-angular-bridge']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  config(['$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    
     $routeProvider.otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
   }]);  
