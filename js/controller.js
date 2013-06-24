3053593122e11c396ed4c27cf7cb107488fb9ebb
Martin Naumann <martin.naumann@centralway.com>
2013-06-24 08:00:30 +0200

    Hiding install button when app is already installed + from-option

--- js/controller.js
+++ js/controller.js
@@ -9,15 +9,30 @@ function DirectionsCtrl($scope, directionsSvc) {
     $scope.connections = [];
     $scope.isLoading = false;
     $scope.loadingState = "Loading...";
+    $scope.showFromField = false;
+    $scope.installable = false;
     
-    $scope.isInstallable = function() {
-        return window.navigator.mozApps !== undefined;
+    if(window.navigator.mozApps) {
+        var req = window.navigator.mozApps.getSelf();
+        req.onsuccess = function() {
+            if(req.result == null) {
+                $scope.$apply(function() {
+                    $scope.installable = true;
+                });
+            }
+        };
+        req.onerror = function() {
+            console.log("ERR");
+            console.log(req.result);
+        }  
     }
+    
+    $scope.isInstallable = function() { return $scope.installable; };
     
     $scope.install = function() {
         console.log("Installation attempt...");
         //TODO Find a better way around this absolute URL problem
-        var request = window.navigator.mozApps.install('http://avgp.github.io/direxio/manifest.webapp');
+        var request = window.navigator.mozApps.install('http://192.168.1.103:8000/manifest.webapp');//'http://avgp.github.io/direxio/manifest.webapp');
         request.onsuccess = function () {
             // Save the App object that is returned
             var appRecord = this.result;
@@ -29,12 +44,16 @@ function DirectionsCtrl($scope, directionsSvc) {
         };    
     }
     
+    $scope.toggleCustomFrom = function() {
+        $scope.showFromField = !$scope.showFromField;
+    };
+    
     $scope.findConnections = function() {
         $scope.isLoading = true;
         if(window.webkitNotifications && window.webkitNotifications.checkPermission() !== 0) {
             window.webkitNotifications.requestPermission();
         }
-        if($scope.from === "") {
+        if($scope.from === "" || $scope.from === undefined) {
             directionsSvc.getConnectionsFromHereTo($scope.destination, function success(connections) {
                 $scope.isLoading = false;
                 console.log(connections);
